fecbf619f58f3e1b7d1a42f7ef5a0dbc0eb463bd
shankarjatin <shankarjatin1005@gmail.com>
2025-03-09 01:25:12 +0530

    done

--- my-next-app/next.config.ts
+++ my-next-app/next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: 'build',
+ 
 
 };
+module.exports = {
+  distDir: 'build',
+}
 
 export default nextConfig;
